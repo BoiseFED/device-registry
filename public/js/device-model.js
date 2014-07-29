f1675e2abd849426f4c26b5452038e9758307b12
Adam Clark <adamclerk@gmail.com>
2014-07-28 23:24:17 -0600

    fixing weird git merge issue

--- public/js/device-model.js
+++ public/js/device-model.js
@@ -2,6 +2,13 @@ define(['backbone', 'bus'], function (Backbone, bus) {
   return Backbone.Model.extend({
     validate: function () {
       return false;
+<<<<<<< HEAD
+=======
+    },
+    parse: function (response) {
+      console.log(response);
+      return response;
+>>>>>>> fixing weird git merge issue
     }
   });
 });
